15a08ba978d6f0ac37b7bf1868fec857349c4d97
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-06-17 12:24:38 +0530

    387 ms, faster than 93.07% and 66.2 MB, less than 9.29%

---
+++ 2885. Rename Columns.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
+
+    students.columns = ['student_id',
+                        'first_name', 'last_name', 'age_in_years']
+
+    return students
+
+    students = students.rename(
+        columns={
+            "id": "student_id",
+            "first": "first_name",
+            "last": "last_name",
+            "age": "age_in_years",
+        }
+    )
+    return students
