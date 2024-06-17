1986dc705fbd599f0b0b02278aa80ddf4dd3f231
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-06-17 12:11:40 +0530

    371 ms, faster than 96.88%  and 65.9 MB, less than 8.87%

---
+++ 2886. Change Data Type.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def changeDatatype(students: pd.DataFrame) -> pd.DataFrame:
+
+    students["grade"] = students["grade"].astype("int")
+
+    return students
+    students = students.astype(
+        {'grade': int}
+    )
+
+    return students
