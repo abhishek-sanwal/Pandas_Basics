64b64591204ce5319b22b2ab3d61b262b6e914ea
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-06-17 12:27:58 +0530

    384 ms, faster than 97.92%  and 66.2 MB, less than 5.87%

---
+++ 2888. Reshape Data: Concatenate.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+
+def concatenateTables(df1: pd.DataFrame, df2: pd.DataFrame) -> pd.DataFrame:
+
+    new_df = pd.concat([df1, df2], axis=0, ignore_index=True)
+
+    return new_df
